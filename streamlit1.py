9d783764cc2e1cf3ac776e6506b9a0fcf4cc2c51
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-09-27 13:19:05 +0530

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -14,4 +14,4 @@ st.dataframe(fruits_to_show)
 st.header('FruityVice Fruit Advice!')
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text (fruityvice_response)
+st.text (fruityvice_response.json())
