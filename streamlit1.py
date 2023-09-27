ebb84cb954ee120f55d51db4048cb1ddd14a1931
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-09-27 11:15:06 +0530

    Update streamlit1.py
    Imported Pandas
    Used CSV File

--- streamlit1.py
+++ streamlit1.py
@@ -3,3 +3,6 @@ st.title('My parent new healthy diner')
 st.header ('Breakfast Menu')
 st.text('Blueberry Oat Meal')
 st.text('🥑Avocado Toast🍞')
+import pandas as pd
+my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+st.dataframe(my_fruit_list)
