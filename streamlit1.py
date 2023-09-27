67a4c38580173fcbabcdcaa2d9804ee97b9e24c6
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-09-27 11:33:33 +0530

    Update streamlit1.py
    Added Filter to display selected data

--- streamlit1.py
+++ streamlit1.py
@@ -7,5 +7,6 @@ import pandas as pd
 my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
 
-st.multiselect("Pick Some Fruits:",list(my_fruit_list.index))
+fruit_selected=st.multiselect("Pick Some Fruits:",list(my_fruit_list.index),['Avocado','Banana'])
+fruits_to_show=my_fruit_list.loc[fruit_selected]
 st.dataframe(my_fruit_list)
