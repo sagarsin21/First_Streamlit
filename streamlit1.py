339fd5596880731145bf803b03f76403cf92bfc7
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-09-27 11:24:47 +0530

    Update streamlit1.py
    Added multiselect index by Fruit Name

--- streamlit1.py
+++ streamlit1.py
@@ -5,4 +5,7 @@ st.text('Blueberry Oat Meal')
 st.text('🥑Avocado Toast🍞')
 import pandas as pd
 my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list=my_fruit_list.set_index('Fruit')
+
+st.multiselect("Pick Some Fruits:",list(my_fruit_list.index))
 st.dataframe(my_fruit_list)
