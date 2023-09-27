1ebb15e7d1202278616407c871c46420a6e6da59
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-09-27 16:06:53 +0530

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -6,15 +6,15 @@ st.text('🥑Avocado Toast🍞')
 import pandas as pd
 my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
-
 fruit_selected=st.multiselect("Pick Some Fruits:",list(my_fruit_list.index),[avocado,banana])
 fruits_to_show=my_fruit_list.loc[fruit_selected]
 st.dataframe(fruits_to_show)
 # New section to display FruityVice API response
 st.header('FruityVice Fruit Advice!')
+fruit_choice=st.text_input('What fruit would you like information about','Kiwi')
+st.write('The user entered',fruit_choice)
 import requests
 fruityvice_response=requests.get("https://fruityvice.com/api/fruit/watermelon")
-
 fruityvice_normalized=pd.json_normalize(fruityvice_response.json())
 #Output as table
 st.dataframe(fruityvice_normalized)
