663e43d9636f1357ddef9037749763e457ada355
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-09-27 16:07:52 +0530

    Update streamlit1.py

--- streamlit1.py
+++ streamlit1.py
@@ -6,7 +6,7 @@ st.text('🥑Avocado Toast🍞')
 import pandas as pd
 my_fruit_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list=my_fruit_list.set_index('Fruit')
-fruit_selected=st.multiselect("Pick Some Fruits:",list(my_fruit_list.index),[avocado,banana])
+fruit_selected=st.multiselect("Pick Some Fruits:",list(my_fruit_list.index),[Avocado,Banana])
 fruits_to_show=my_fruit_list.loc[fruit_selected]
 st.dataframe(fruits_to_show)
 # New section to display FruityVice API response
