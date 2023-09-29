4ecec7119ec5dce9237cb42adea8acb078bd847d
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-09-29 16:47:34 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,20 +1,27 @@
 import streamlit as st
-st.title('ARCHITECTURE')
-if st.button('Submit'):
-  st.write('Submittedd Successfully')
-name =st.text_input('Name')
-st.write(name)
-
-address=st.text_area('Enter your address')
-st.write(address)
-date=st.date_input('Enter a Date')
-time=st.time_input('Enter a Time')
-if st.checkbox("You accept T&C",value=False):
-  st.write('Thank You')
-
-st.radio('Colours',['r','g','b'],index=0)
-st.selectbox('Colours',['r','g','b'],index=1)
-st.number_input('Numbers',min_value=18,max_value=100,value=25)
-
+import pyodbc
+@st.cache_resource
+def init_connection():
+  return pyodbc.connect(
+    "Driver={ODBC Driver 17 for SQL Server};SERVER="
+    + st.secrets["server"]
+    +";DATABASE="
+    +st.secrets["database"]
+    +";UID="
+    st.secrets["username"]
+    +";PWD="
+    +st.secrets
+  )
+conn=init_connection()
+@st.cache_data(ttl=600)
+def run_query(query):
+  with conn.cursor() as cur:
+    cur.execute(query)
+    return cur.fetchall()
+rows=run_query("Select * from mytable;")
+#print results
+for row in rows:
+  st.write(f"{row[0]} has a :{row[1]}:")
+  
 
 
