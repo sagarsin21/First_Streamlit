6eda24f1d6c7999f5f46b07b9c9d4d3c57275a71
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-09-29 12:12:32 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -3,7 +3,7 @@ st.title('ARCHITECTURE')
 if st.button('Submit'):
   st.write('Submittedd Successfully')
 name =st.text_input('Name')
-st.write(name)
+st.write(name);
 
 address=st.text_area('Enter your address')
 st.write(address)
