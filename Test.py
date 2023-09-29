b1a904c7a81a94f6536668b60b48f96d1253b1f1
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-09-29 12:36:48 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -9,6 +9,5 @@ address=st.text_area('Enter your address')
 st.write(address)
 date=st.date_input('Enter a Date')
 time=st.time_input('Enter a Time')
-st.write(date,time)
-with st.echo():
- st.write('Code will be executed and printed')
+
+
