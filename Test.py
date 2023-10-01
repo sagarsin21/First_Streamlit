7921dffb758cac3fceb173a09d0020a6b9455605
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-01 22:26:45 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,27 +1,33 @@
 import streamlit as st
 import pyodbc
+
+# Initialize connection.
+# Uses st.cache_resource to only run once.
 @st.cache_resource
 def init_connection():
-  return pyodbc.connect(
-    "Driver={ODBC Driver 17 for SQL Server};SERVER="
-    + st.secrets["server"]
-    +";DATABASE="
-    +st.secrets["database"]
-    +";UID="
-    +st.secrets["username"]
-    +";PWD="
-    +st.secrets
-  )
-conn=init_connection()
+    return pyodbc.connect(
+        "DRIVER={ODBC Driver 17 for SQL Server};SERVER="
+        + st.secrets["localhost"]
+        + ";DATABASE="
+        + st.secrets["mydb"]
+        + ";UID="
+        + st.secrets["SA"]
+        + ";PWD="
+        + st.secrets["1234"]
+    )
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
 @st.cache_data(ttl=600)
 def run_query(query):
-  with conn.cursor() as cur:
-    cur.execute(query)
-    return cur.fetchall()
-rows=run_query("Select * from mytable;")
-#print results
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
 for row in rows:
-  st.write(f"{row[0]} has a :{row[1]}:")
-  
-
-
+    st.write(f"{row[0]} has a :{row[1]}:")
