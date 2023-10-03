04a5f05b973b9b64086ebd1c90f21ac2e82b05af
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 17:09:19 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Excel Data Entry')
-name = st.text_input("Enter your name:")
-age = st.number_input("Enter your age:")
-email = st.text_input("Enter your email:")
-st.dataframe(name,age,email)
+st.title('SMFG Reporting App')
+# Get the username from user input
+username = None
+username = st.text_input('Username', key='username')
+# User input fields
+
+    if not username:
+        st.error('Username cannot be empty. Please enter your username.')
+    else:
+        # Process the form submission
+        st.write(f"Hello, {username}! Your {reports} will be placed in the shared folder in a few hours. Kindly wait. Thank you!")
