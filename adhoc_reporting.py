23bb7ecc1453e6c28f7d3bff64ce634bddb78736
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 17:07:59 +0530

    Update adhoc_reporting.py

--- adhoc_reporting.py
+++ adhoc_reporting.py
@@ -19,26 +19,7 @@ st.title('SMFG Reporting App')
 username = None
 username = st.text_input('Username', key='username')
 # User input fields
-with st.container():
-    col1, col2 = st.columns(2)  # Create two columns
 
-    from_date = col1.date_input('From Date', key='from_date')
-    to_date = col2.date_input('To Date', key='to_date')
-    
-    customer_id = col1.text_input('Customer ID (comma-separated)', key='customer_id')
-    batch_id = col2.text_input('Batch ID (comma-separated)', key='batch_id')
-
-    app_ref_no = col1.text_input('App Ref No (comma-separated)', key='app_ref_no')
-    lead_id = col2.text_input('Lead ID (comma-separated)', key='lead_id')
-
-    gl_codes = col1.text_input('GL Code (comma-separated)', key='gl_codes')
-    lan = col2.text_input('LAN (comma-separated)', key='lan')
-# Report selection dropdown
-reports = st.selectbox('Select a report', ['Report 1', 'Report 2', 'Report 3'])
-
-# Submit button
-if st.button('Submit'):
-    #username = st.session_state.username  # Set username (you should set this value as needed)
     if not username:
         st.error('Username cannot be empty. Please enter your username.')
     else:
