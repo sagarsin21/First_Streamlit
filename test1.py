a45eff1a9a296d8182376b2ded1d4f24224fca78
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 17:59:59 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -4,6 +4,4 @@ import pandas as pd
 st.title('SMFG Reporting App')
 username = None
 username = st.text_input('Username', key='username')
-    if not username:
-        st.error('Username cannot be empty. Please enter your username.')
-    
+  
