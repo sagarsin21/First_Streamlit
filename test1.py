f7355af807180c6fea059d059ebd247b162181ce
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 16:38:30 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-
-    st.title("Streamlit Excel Data Entry")
+st.title("Streamlit Excel Data Entry")
 
     # Get user input
   #  name = st.text_input("Enter your name:")
