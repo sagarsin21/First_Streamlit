2fdae1688accf254f1fd4581545649a36a46e880
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 11:53:46 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,10 +1,10 @@
 import pyodbc 
-server ='localhost' 
+server ='SAGAR' 
 database='mydb'
 username='SA'
 password='1234'
 
 # Create connection string
-connection_string=f'DRIVER=SQL Server;SERVER={localhost};DATABASE={mydb};UID={SA};PWD={1234}'
+connection_string=f'DRIVER=SQL Server;SERVER={SAGAR};DATABASE={mydb};UID={SA};PWD={1234}'
 # Connect to the database
 conn=pyodbc.connect(connection_string)
