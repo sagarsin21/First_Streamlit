ffada066f7b6fdb6fbc4452039184db954ef81eb
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 16:45:28 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -2,3 +2,6 @@ import streamlit as st
 import pandas as pd
 
 st.title('Excel Data Entry')
+name = st.text_input("Enter your name:")
+age = st.number_input("Enter your age:")
+email = st.text_input("Enter your email:")
