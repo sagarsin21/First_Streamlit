ce7c7fde29bf099c9dcca9d05220cf1746007d8a
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 16:35:36 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -10,16 +10,16 @@ import pandas as pd
     email = st.text_input("Enter your email:")
 
     # Create a DataFrame with the user input
-    data = {'Name': [name], 'Age': [age], 'Email': [email]}
-    df = pd.DataFrame(data)
+   # data = {'Name': [name], 'Age': [age], 'Email': [email]}
+    #df = pd.DataFrame(data)
 
     # Display the DataFrame
-    st.write("Entered Data:", df)
+   # st.write("Entered Data:", df)
 
     # Add a submit button
-    if st.button("Submit"):
+   # if st.button("Submit"):
         # Write the data to an Excel file
-        df.to_excel("user_data.xlsx", index=False)
-        st.success("Data submitted successfully!")
-        if __name__ == "__main__":
-    main()
+      #  df.to_excel("user_data.xlsx", index=False)
+       # st.success("Data submitted successfully!")
+     #   if __name__ == "__main__":
+    #main()
