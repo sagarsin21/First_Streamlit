f0d329f2d6f4e41d71762e1a8d427e8709b9722f
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 17:12:28 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,4 +1,9 @@
 import streamlit as st
 import pyodbc
 import pandas as pd
-
+st.title('SMFG Reporting App')
+username = None
+username = st.text_input('Username', key='username')
+    if not username:
+        st.error('Username cannot be empty. Please enter your username.')
+    else:
