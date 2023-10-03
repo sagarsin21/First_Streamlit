298000a8e46cfca824912d79bcabe526b315ab02
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 16:44:36 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,23 +1,4 @@
 import streamlit as st
 import pandas as pd
 
-#st.title("Streamlit Excel Data Entry")
-    # Get user input
-name = st.text_input("Enter your name:")
-age = st.number_input("Enter your age:")
-email = st.text_input("Enter your email:")
-
-    # Create a DataFrame with the user input
-data = {'Name': [name], 'Age': [age], 'Email': [email]}
-df = pd.DataFrame(data)
-
-    # Display the DataFrame
-st.write("Entered Data:", df)
-
-    # Add a submit button
-if st.button("Submit"):
-        # Write the data to an Excel file
-      df.to_excel("user_data.xlsx", index=False)
-    st.success("Data submitted successfully!")
-       if __name__ == "__main__":
-    main()
+st.title('Excel Data Entry')
