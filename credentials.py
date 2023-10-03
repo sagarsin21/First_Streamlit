efad7bf2c6399e83304f6b97722a210c2e1464f2
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 12:11:13 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,10 +1,10 @@
 import pyodbc 
-server ='SAGAR(SQL Server 14.0.1000.169 -sa)' 
+server ='DESKTOP-J1I1PPA' 
 database='mydb'
 username='SA'
 password='1234'
 
 # Create connection string
-connection_string=f'DRIVER=SQL Server;SERVER={SAGAR(SQL Server 14.0.1000.169 -sa)};DATABASE={mydb};UID={SA};PWD={1234}'
+connection_string=f'DRIVER=SQL Server;SERVER={DESKTOP-J1I1PPA};DATABASE={mydb};UID={SA};PWD={1234}'
 # Connect to the database
 conn=pyodbc.connect(connection_string)
