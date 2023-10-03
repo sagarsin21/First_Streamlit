faa57cb8f699845f41096e3cc43025631a0411cb
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-03 12:29:37 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,8 @@
 import streamlit as st
 connection_string = 'DRIVER={SQL Server};SERVER=DESKTOP-J1I1PPA;DATABASE=mydb;UID=SA;PWD=1234'
 st.header('Hello')
+import pyodbc
+conn = pyodbc.connect(connection_string)
+import pandas as pd
+query = 'SELECT * FROM myTable'
+df = pd.read_sql(query, conn)
