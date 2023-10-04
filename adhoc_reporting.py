638f02c1e65104671c7d3b2d5431f3c36672971b
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-04 11:17:38 +0530

    Update adhoc_reporting.py

--- adhoc_reporting.py
+++ adhoc_reporting.py
@@ -2,18 +2,7 @@ import streamlit as st
 import pyodbc
 import pandas as pd
 
-# Database connection setup
-#conn = pyodbc.connect(
-#    'DRIVER={ODBC Driver 17 for SQL Server};'
-#    'SERVER=LocalHost;'
-#    'DATABASE=LMS;'
-#    'UID=service_account;'
-#    'PWD=pwd;'
-#)
 
-#cursor = conn.cursor()
-
-# Streamlit UI
 st.title('SMFG Reporting App')
 # Get the username from user input
 username = None
@@ -23,5 +12,5 @@ username = st.text_input('Username', key='username')
     if not username:
         st.error('Username cannot be empty. Please enter your username.')
     else:
-        # Process the form submission
+        
         st.write(f"Hello, {username}! Your {reports} will be placed in the shared folder in a few hours. Kindly wait. Thank you!")
