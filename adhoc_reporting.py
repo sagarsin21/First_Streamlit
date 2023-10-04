210c99f7911cd4bfa71ddb07f41b13f595fcadba
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-04 11:33:33 +0530

    Update adhoc_reporting.py

--- adhoc_reporting.py
+++ adhoc_reporting.py
@@ -1,14 +1,6 @@
 import streamlit as st
 import pyodbc
 import pandas as pd
-
-
-st.title('SMFG Reporting App')
-# Get the username from user input
-username = None
-username = st.text_input('Username', key='username')
-# User input fields
-    if not username:
-        st.error('Username cannot be empty. Please enter your username.')
-    else:    
-        st.write(f"Hello, {username}! Your {reports} will be placed in the shared folder in a few hours. Kindly wait. Thank you!")
+st.title('SMFG Reprorting')
+username=None
+username=st.text_input('username',key='username')
