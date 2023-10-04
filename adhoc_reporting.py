64e4933c76a7c3a0e17efa6433214ca0069f2eef
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-04 11:55:11 +0530

    Update adhoc_reporting.py

--- adhoc_reporting.py
+++ adhoc_reporting.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pyodbc
 import pandas as pd
 st.title('SMFG Reporting')
+st.header('Adhoc Reporting')
 username=None
 username=st.text_input('username',key='username')
 df=pd.dataframe([a,b])
