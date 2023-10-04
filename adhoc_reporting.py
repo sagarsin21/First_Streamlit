ad8ce09dc7b6d599fa7705e13eede82265bc8d68
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-04 13:17:32 +0530

    Update adhoc_reporting.py

--- adhoc_reporting.py
+++ adhoc_reporting.py
@@ -1,8 +1,26 @@
 import streamlit as st
-import pyodbc
 import pandas as pd
-st.title('SMFG Reporting')
-st.header('Adhoc Reporting')
-username=None
-username=st.text_input('username',key='username')
-st.button('Submit')
+
+def main():
+    st.title("Streamlit Excel Data Entry")
+
+    # Get user input
+    name = st.text_input("Enter your name:")
+    age = st.number_input("Enter your age:")
+    email = st.text_input("Enter your email:")
+
+    # Create a DataFrame with the user input
+    data = {'Name': [name], 'Age': [age], 'Email': [email]}
+    df = pd.DataFrame(data)
+
+    # Display the DataFrame
+    st.write("Entered Data:", df)
+
+    # Add a submit button
+    if st.button("Submit"):
+        # Write the data to an Excel file
+        df.to_excel("user_data.xlsx", index=False)
+        st.success("Data submitted successfully!")
+
+if __name__ == "__main__":
+    main()
