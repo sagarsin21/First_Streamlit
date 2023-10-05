92d288634692a5d59371a1f7b7c2eec7d30c33e3
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-05 12:51:04 +0530

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,26 +1,32 @@
 import streamlit as st
 import pandas as pd
-import openpyxl
+
 def main():
-    st.title("Streamlit Excel Data Entry")
+    st.title("Streamlit Login and Data Export App")
 
-    # Get user input
-    name = st.text_input("Enter your name:")
-    age = st.number_input("Enter your age:")
-    email = st.text_input("Enter your email:")
+    # Input for username
+    username = st.text_input("Username:")
 
-    # Create a DataFrame with the user input
-    data = {'Name': [name], 'Age': [age], 'Email': [email]}
+    # Input for password (password input field for security)
+    password = st.text_input("Password:", type="password")
+
+    # Submit button
+    if st.button("Submit"):
+        # Check if the username and password are not empty
+        if username and password:
+            # Export data to Excel
+            export_data_to_excel(username, password)
+            st.success("Data exported to Excel successfully!")
+        else:
+            st.error("Please enter both username and password.")
+
+def export_data_to_excel(username, password):
+    # Example: Creating a Pandas DataFrame with the input data
+    data = {'Username': [username], 'Password': [password]}
     df = pd.DataFrame(data)
 
-    # Display the DataFrame
-    st.write("Entered Data:", df)
-
-    # Add a submit button
-    if st.button("Submit"):
-        # Write the data to an Excel file
-        df.to_excel("user_data.xlsx", index=False)
-        st.success("Data submitted successfully!")
+    # Save the DataFrame to an Excel file
+    df.to_excel('user_data.xlsx', index=False)
 
 if __name__ == "__main__":
     main()
