57410d511e98c016d39c62bb99691789b4cda5fa
sagarsin21 <102144547+sagarsin21@users.noreply.github.com>
2023-10-05 13:33:11 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,7 +1,3 @@
+import openpyxl
 import streamlit as st
-import pyodbc
-import pandas as pd
-st.title('SMFG Reporting App')
-username = None
-username = st.text_input('Username', key='username')
-  
+st.header('Test')
